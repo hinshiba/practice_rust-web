5e91fa84d63d3e44617ea0098cf81090944567d8
hinshiba <105423175+hinshiba@users.noreply.github.com>
2024-11-15 22:02:17 +0900

    feat: update and delete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use axum::{
-    extract::{Query, State}, http::StatusCode, routing::{get, post}, Json, Router
+    extract::{Path, Query, State}, http::StatusCode, routing::{delete, get, patch, post}, Json, Router
 };
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
-use std::{collections::HashMap, env};
+use std::{collections::HashMap, env, os::windows::process};
 use std::sync::Arc;
 
 #[tokio::main]
@@ -17,8 +17,10 @@ async fn main() {
     let pool = Arc::new(SqlitePool::connect(&database_url).await.unwrap());
     // build our application with a route
     let app = Router::new()
+        .route("/equip", get(get_items))
         .route("/equip", post(create_item))
-        .route("/equip", get(get_item))
+        .route("/equip", patch(update_item))
+        .route("/equip/:id", delete(delete_item))
         .with_state(pool.clone());
 
     // run our app with hyper, listening globally on port 3000
@@ -26,7 +28,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn get_item(
+async fn get_items(
     State(pool): State<Arc<SqlitePool>>,  
     Query(params): Query<HashMap<String, String>>  
 ) -> (StatusCode, Json<Vec<EquipData>>) {
@@ -43,8 +45,8 @@ async fn get_item(
 async fn create_item(
     State(pool): State<Arc<SqlitePool>>,
     Json(payload): Json<CreateItem>,
-) -> (StatusCode, Json<IDReturn>) {
-    let idr = sqlx::query_as!(IDReturn, 
+) -> (StatusCode, Json<ItemID>) {
+    let idr = sqlx::query_as!(ItemID, 
         "INSERT INTO equips (applicant_name, disposaler_name, equipment_type, equipment_name, purchase_url, cost) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id", 
         payload.applicant_name, payload.disposaler_name, payload.equipment_type, payload.equipment_name, payload.purchase_url, payload.cost)
     .fetch_one(&*pool).await;
@@ -52,6 +54,36 @@ async fn create_item(
         Ok(r ) => (StatusCode::CREATED, Json(r)),
         Err(e) => panic!("{}", e),
     }
+}
+
+async fn update_item(
+    State(pool): State<Arc<SqlitePool>>,
+    Json(payload): Json<UpdateItem>,
+) -> StatusCode {
+    let UpdateItem { id, disposaler_name, situation } = payload;
+    if let Some(disposaler_name) = disposaler_name {
+        match sqlx::query!(r#"UPDATE equips SET disposaler_name = $1 WHERE id = $2"#, disposaler_name, id).execute(&*pool).await {
+            Ok(r) if r.rows_affected() != 0 => (), // OK 次のリクエスト
+            Ok(_) => return StatusCode::NOT_FOUND,
+            Err(_) => return StatusCode::BAD_REQUEST,
+        } 
+    }
+    // if let Some(situation) = situation {
+    //     sqlx::query!(r#"UPDATE equips SET situation = $1 WHERE id = $2"#, situation, id).execute(&*pool).await;
+    // }
+    return StatusCode::OK;
+}
+
+async fn delete_item(
+    State(pool): State<Arc<SqlitePool>>,
+    Path(id): Path<i64>
+) -> StatusCode {
+    print!("{}", id);
+    match sqlx::query!(r#"DELETE FROM equips WHERE id = $1"#, id).execute(&*pool).await {
+        Ok(r) if r.rows_affected() != 0 => return StatusCode::OK,
+        Ok(_) => return StatusCode::NOT_FOUND,
+        Err(_) => return StatusCode::BAD_REQUEST,
+    } 
 }
 
 #[derive(Deserialize)]
@@ -64,8 +96,15 @@ struct CreateItem {
     cost: i64,
 }
 
-#[derive(Serialize)]
-struct IDReturn {
+#[derive(Deserialize)]
+struct UpdateItem {
+    id: i64,
+    disposaler_name: Option<String>,
+    situation: Option<Situation>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ItemID {
     id: i64,
 }
 

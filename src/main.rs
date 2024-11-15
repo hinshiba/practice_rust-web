0a51e23b6dd40fe50dc993023b5bc1319e162efd
hinshiba <105423175+hinshiba@users.noreply.github.com>
2024-11-15 18:59:33 +0900

    feat: get all

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use axum::{
-    extract::State, http::StatusCode, routing::{get, post}, Json, Router
+    extract::{Query, State}, http::StatusCode, routing::{get, post}, Json, Router
 };
 use serde::{Deserialize, Serialize};
 use sqlx::SqlitePool;
-use std::env;
+use std::{collections::HashMap, env};
 use std::sync::Arc;
 
 #[tokio::main]
@@ -16,11 +16,27 @@ async fn main() {
 
     let pool = Arc::new(SqlitePool::connect(&database_url).await.unwrap());
     // build our application with a route
-    let app = Router::new().route("/equip", post(create_item)).with_state(pool.clone());
+    let app = Router::new()
+        .route("/equip", post(create_item))
+        .route("/equip", get(get_item))
+        .with_state(pool.clone());
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn get_item(
+    State(pool): State<Arc<SqlitePool>>,  
+    Query(params): Query<HashMap<String, String>>  
+) -> (StatusCode, Json<Vec<EquipData>>) {
+    let data = sqlx::query_as!(EquipData, 
+    r##"SELECT id, applicant_name, disposaler_name, equipment_type, equipment_name, purchase_url, cost, situation as "situation: Situation" , approval_date FROM equips"##)
+    .fetch_all(&*pool).await;
+    match data {
+        Ok(r ) => (StatusCode::OK, Json(r)),
+        Err(e) => panic!("{}", e),
+    }
 }
 
 
@@ -45,10 +61,33 @@ struct CreateItem {
     equipment_type: String,
     equipment_name: String,
     purchase_url: String,
-    cost: u32,
+    cost: i64,
 }
 
 #[derive(Serialize)]
 struct IDReturn {
     id: i64,
+}
+
+#[derive(Debug, Deserialize, Serialize, sqlx::Type)]
+#[sqlx(rename_all = "lowercase")] // SQL の値と対応させる
+enum Situation {
+    Pending,
+    Approved,
+    Purchased,
+    Equipment,
+}
+
+
+#[derive(Serialize)]
+struct EquipData {
+    id: i64,
+    applicant_name: String,
+    disposaler_name: String,
+    equipment_type: String,
+    equipment_name: String,
+    purchase_url: String,
+    cost: i64,
+    situation: Situation,
+    approval_date: Option<String>,
 }
